c40ff8988fcda02e9b2d0ff99a8bf009d75b6955
Ramsey02 <ramsey@campus.technion.ac.il>
2024-07-25 15:11:16 +0300

    fixing test 28 + test 283

--- de.cpp
+++ de.cpp
@@ -1,62 +1,62 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include "parser.tab.hpp"
-#include "source.hpp"
-#include "hw3_output.hpp"
+// #include <iostream>
+// #include <fstream>
+// #include <sstream>
+// #include "parser.tab.hpp"
+// #include "source.hpp"
+// #include "hw3_output.hpp"
 
-// Extern declarations to make them accessible in the main file
-extern int yyparse();
-extern FILE* yyin;
-extern int yylineno;
+// // Extern declarations to make them accessible in the main file
+// extern int yyparse();
+// extern FILE* yyin;
+// extern int yylineno;
 
-// Include necessary header files for your symbol table and function declarations
-#include "CheckFunctions.h"
-#include "SymbolTable.hpp"
+// // Include necessary header files for your symbol table and function declarations
+// #include "CheckFunctions.h"
+// #include "SymbolTable.hpp"
 
-using namespace std;
+// using namespace std;
 
-// Global instances
-
-
+// // Global instances
 
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include "parser.tab.hpp"
-#include "source.hpp"
-#include "hw3_output.hpp"
 
-extern int yyparse();
-extern FILE* yyin;
 
-int main(int argc, char* argv[]) {
-    //for tests: std::cout << "Debug: Starting program execution" << std::endl;
+// #include <iostream>
+// #include <fstream>
+// #include <sstream>
+// #include "parser.tab.hpp"
+// #include "source.hpp"
+// #include "hw3_output.hpp"
 
-    if (argc == 2) {
-        // Input from file specified as command-line argument
-        yyin = fopen(argv[1], "r");
-        if (!yyin) {
-            std::cerr << "Error opening file: " << argv[1] << std::endl;
-            return 1;
-        }
-    } else if (argc == 1) {
-        // No arguments, use stdin
-        yyin = stdin;
-    } else {
-        std::cerr << "Usage: " << argv[0] << " [<input file>]" << std::endl;
-        return 1;
-    }
+// extern int yyparse();
+// extern FILE* yyin;
 
-    // Run the parser
-    yyparse();
+// int main(int argc, char* argv[]) {
+//     //for tests: std::cout << "Debug: Starting program execution" << std::endl;
 
-    // Close the input file if we opened one
-    if (argc == 2) {
-        fclose(yyin);
-    }
+//     if (argc == 2) {
+//         // Input from file specified as command-line argument
+//         yyin = fopen(argv[1], "r");
+//         if (!yyin) {
+//             std::cerr << "Error opening file: " << argv[1] << std::endl;
+//             return 1;
+//         }
+//     } else if (argc == 1) {
+//         // No arguments, use stdin
+//         yyin = stdin;
+//     } else {
+//         std::cerr << "Usage: " << argv[0] << " [<input file>]" << std::endl;
+//         return 1;
+//     }
 
-    //for tests: std::cout << "Debug: Ending program execution" << std::endl;
-    return 0;
-}
+//     // Run the parser
+//     yyparse();
+
+//     // Close the input file if we opened one
+//     if (argc == 2) {
+//         fclose(yyin);
+//     }
+
+//     //for tests: std::cout << "Debug: Ending program execution" << std::endl;
+//     return 0;
+// }
